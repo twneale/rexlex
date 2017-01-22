6c451a3b7e9134cbdf895a7ec5682e480480ef1a
Thom Neale <twneale@gmail.com>
2017-01-21 22:59:14 -0500

    added cached attr

---
+++ rexlex/utils/cachedattr.py
@@ -0,0 +1,34 @@
+class CachedAttr(object):
+    '''Computes attr value and caches it in the instance.'''
+
+    def __init__(self, method, name=None):
+        self.method = method
+        self.name = name or method.__name__
+
+    def __get__(self, inst, cls):
+        if inst is None:
+            return self
+        result = self.method(inst)
+        setattr(inst, self.name, result)
+        return result
+
+
+class CachedClassAttr(object):
+    '''Computes attribute value and caches it in class.
+
+    Example:
+        class MyClass(object):
+            def myMethod(cls):
+                # ...
+            myMethod = CachedClassAttribute(myMethod)
+    Use "del MyClass.myMethod" to clear cache.'''
+
+    def __init__(self, method, name=None):
+        self.method = method
+        self.name = name or method.__name__
+
+    def __get__(self, inst, cls):
+        result = self.method(cls)
+        setattr(cls, self.name, result)
+        return result
+
